f7684bb1eb504cf4f2bd2a8f1f6e55c939f47882
Tareq <tareqnmd@gmail.com>
2025-09-27 20:05:15 +0600

    Refactor AppLayout to wrap content in a container for improved layout structure; remove redundant styles from global CSS.

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -9,9 +9,11 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
 				color="var(--primary)"
 				showSpinner={false}
 			/>
-			<Header />
-			<main>{children}</main>
-			<Footer />
+			<div className="container px-4 py-8 mx-auto grid grid-rows-[auto_1fr_auto] min-h-screen gap-8">
+				<Header />
+				<main>{children}</main>
+				<Footer />
+			</div>
 		</>
 	);
 };
