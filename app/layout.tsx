b139725c1cadb6f8a5304e2bdd016dda67764a0f
Tareq <tareqnmd@gmail.com>
2025-09-27 23:43:02 +0600

    Add suppressHydrationWarning to RootLayout for improved hydration handling in Next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
+				suppressHydrationWarning
 				id="chess-t32"
 				className={`${font.variable} antialiased`}
 			>
