61d2ef635c866dcb1142a9a07eccfdfa1d7310f4
Tareq <tareqnmd@gmail.com>
2025-09-27 20:02:21 +0600

    Add nextjs-toploader package and integrate it into AppLayout for improved loading experience; update global styles to define primary color.

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -1,9 +1,14 @@
+import NextTopLoader from 'nextjs-toploader';
 import Footer from '../common/Footer';
 import Header from '../common/Header';
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<>
+			<NextTopLoader
+				color="var(--primary)"
+				showSpinner={false}
+			/>
 			<Header />
 			<main>{children}</main>
 			<Footer />
