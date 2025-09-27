6304086c5747169ec5e9d9dbea805e70233aa865
Tareq <tareqnmd@gmail.com>
2025-09-27 18:49:44 +0600

    Refactor ErrorComponent for improved styling and layout; updated class names and structure for better user experience.

--- components/common/ErrorComponent.tsx
+++ components/common/ErrorComponent.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 const ErrorComponent = ({ reset }: { reset: () => void }) => {
-  return (
-    <div className="error-page">
-      <h2 className="error-page-header">Something went wrong!</h2>
-      <button className="error-page-button" onClick={() => reset()}>
-        Try again
-      </button>
-    </div>
-  );
+	return (
+		<div className="grid place-content-center gap-1 h-full text-center">
+			<h2 className="text-xl font-bold">Something went wrong!</h2>
+			<button
+				className="underline"
+				onClick={() => reset()}
+			>
+				Try again
+			</button>
+		</div>
+	);
 };
 
 export default ErrorComponent;
